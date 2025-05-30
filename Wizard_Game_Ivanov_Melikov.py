31132f9dbfcee41e1178aee750e20d10fc2c196e
Aleksei <alexxxey.ivanovvv@gmail.com>
2025-05-30 13:50:45 +0300

    Add files via upload

--- Wizard_Game_Ivanov_Melikov.py
+++ Wizard_Game_Ivanov_Melikov.py
@@ -1,24 +1,51 @@
-#Wizard game. Made by Aleksei Ivanov and Ilja Melikov
-#Remake первоначального проекта в Scratch https://scratch.mit.edu/projects/1130069789/editor/
+import pygame
 
-# Должна быть переменная, которая определяет какой уровень сейчас в игре. От неё зависит фон и появление NPC на экране
+pygame.init()
+gameover = False
 
-# В игре всегда играет музыка.
+screenX = 1200
+screenY = 800
+screen=pygame.display.set_mode([screenX,screenY])
+pygame.display.set_caption("keyboard input")
+screen.fill(lBlue)# заменить  на пнг от уровней
+clock = pygame.time.Clock()
 
-# Переменная Fireball. Должен быть небольшой таймаут между стрельбой. Fireball исчезает при столкновении с противником или заходе до конца экрана. Имеет звук.
+Wizard = 0 #add png
+Wizard_Move = 0 #add move anim when use fireball or shield
+Wizard_Block_Move = 0 #Block movement while in dialogue
+Wizard_Death = 0 #If wizard dies rotate 90 degrees
+Wizard_X = 0 #move
+Wizard_Y = 0 #jump
+Fireball = 0 #add 2 seconds timeout
+Shield = 0 #holds for 5 sec, 5 sec timeout recharge
+Shield_Recharge = 0 #Recharge timer
+Level = 0 # 0 - welcome 1 - tutorial 2 - walk level 3 - zombie fight 4 - skeleton fight 5 - frog good luck 6 boss fight
 
-# Противники имеют переменную, которая считает сколько ударов по ним нанесено.
+Frog = 0 #Add frog png
+Frog_Dial = 0 #Frog Dialog box
+Zombie = 0 #Add zombie png
+Zombie_Move_X = 0 #Zombie horizontal movement
+Zombie_Hit = 0 #Add zombie hit counter. if 1 zombie dies
+Zombie_Death = 0 #Zombie dead animation rotate 90 degrees
+Skeleton_1 = 0 #Add zombie png
+Skeletion_1_Move_X = 0 #Skeleton horizontal movement
+Skeleton_1_Hit = 0 #Skeleton hit counter. 2 = death
+Skeleton_1_Death = 0 #Rotate 90 degrees
+Skeleton_2 = 0 #Add zombie png
+Skeletion_2_Move_X = 0 #Skeleton horizontal movement
+Skeleton_2_Hit = 0 #Skeleton hit counter. 2 = death
+Skeleton_2_Death = 0 #Rotate 90 degrees
+Anti_Wiz = 0 #Add enemy wizard png
+Anti_Wiz_Dial = 0 #Anti wiz dialog box
+Anti_Wiz_Fireball = 0 #Add enemy wizard fireball/lighting
+Anti_Wiz_Shield = 0 #Add enemy wizard shield
+Anti_Wiz_Death = 0 #3 hits = death = rotate 90 degrees
 
-# Переменная щита. Щит может бить противников, действует 3-5 секунд, перезарядка тоже около 5 секунд. Активность щита и перезарядка должны показываться на экране. Имеет звук.
 
-# Диалоги. Кнопка E для прогресса по ветви диалога. Во время диалогов движение не доступно.
 
-# 0. Уровень - заствка с названием игры и нашими именами. "НАЖМИТЕ Е ЧТОБЫ НАЧАТЬ ИГРУ"
+while gameover == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
 
-# 1. Уровень - лягуха рассказывает управление. Лягушка ждёт пока ты не выполнишь действия которая она требует.
 
-# 2. Уровень - просто филлерный проход по атмосферной локации
-
-# 3. Уровень - первый противник - зомби, двигается медленно, умирает от 1 файрболла.
-
-# 4. Уровень - появление босса с его диалогом. !!Можно добавить варианты!!, во время диалога движение и удары заблокированы. После диалога босс перед боем включает свой щит на несколько секунд. Босс стреляет в тебя молниями с промежутком по времени.
